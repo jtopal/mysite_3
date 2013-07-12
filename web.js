18a0601ee31d6cba629142d8dab6c400f33a758b
jtopal <jtopal@gmail.com>
2013-07-12 00:33:35 +0000

    hi

--- web.js
+++ web.js
@@ -5,15 +5,10 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 var fs = require('fs');
-var buffer = new buffer (fs.readFileSync('index.html'), 'utf-8');
+var content = fs.readFileSync('index.html', 'utf-8');
   response.send(buf.toString());
  });
 
-//app.get('/', function(request, response) {
-  //  var fs = require ('fs');
-    //fs.readFile('index.html',function(err,data){
-  //console.log(data.toString()); //replace console.log with the code to write to the response.
-//});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
