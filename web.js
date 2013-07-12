8e3f19caf2bf2b6228af1fd199300dfea4764c41
jtopal <jtopal@gmail.com>
2013-07-12 00:27:11 +0000

    add again

--- web.js
+++ web.js
@@ -2,11 +2,18 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+
 app.get('/', function(request, response) {
-    var fs = require ('fs');
-    fs.readFile('index.html',function(err,data){
-  console.log(data.toString()); //replace console.log with the code to write to the response.
-});
+var fs = require('fs');
+var buffer = new buffer ();
+  response.send(buffer.toString('utc 8', fs.readFileSync("index.html")))});
+ });
+
+//app.get('/', function(request, response) {
+  //  var fs = require ('fs');
+    //fs.readFile('index.html',function(err,data){
+  //console.log(data.toString()); //replace console.log with the code to write to the response.
+//});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
