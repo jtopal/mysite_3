8ec020e41170990e91ca9f29c56c6b3e4e0c269c
jtopal <jtopal@gmail.com>
2013-08-27 20:23:41 +0000

    add

--- web.js
+++ web.js
@@ -1,7 +1,8 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-
+var about_html = 'about.html';
+var contact_html = 'contact.html';
 
 app.get('/', function(request, response) {
 var fs = require('fs');
@@ -10,12 +11,12 @@ var buf = new Buffer (fs.readFileSync('index.html'), 'utf-8');
 
 app.get('/about', function(request, response) {
 var fs = require('fs');
-var buf = new Buffer (fs.readFileSync('about.html'), 'utf-8');
+var buf = new Buffer (fs.readFileSync('about_html'), 'utf-8');
   response.send(buf.toString());
 
 app.get('/contact', function(request, response) {
 var fs = require('fs');
-var buf = new Buffer (fs.readFileSync('contact.html'), 'utf-8');
+var buf = new Buffer (fs.readFileSync('contact_html'), 'utf-8');
   response.send(buf.toString());
 
  });
